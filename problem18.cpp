14068d0772b9cea8ff2d0709cf5981c691ce5bb5
Harshaa S <harshaas21@gmail.com>
2026-07-18 09:44:38 +0530

    Hackerrank problem 4

---
+++ problem18.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        int num;
+        cin >> num;
+        arr[i] = num;
+    }
+
+    for(int i=n-1; i>=0; i--){
+        cout << arr[i] <<" ";
+    }
+    return 0;
+}
