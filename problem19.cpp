34c019a7f8059c7bccdd007a24c0098277275c24
Harshaa S <harshaas21@gmail.com>
2026-07-18 11:39:05 +0530

    Hackerrank problem 5

--- problem19.cpp
+++ problem19.cpp
@@ -4,22 +4,19 @@
 using namespace std;
 
 int main() {
-    // Optimize standard I/O operations for competitive programming
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int n, q;
     cin >> n >> q;
 
-    // Create a vector of vectors containing 'n' empty inner vectors
     vector<vector<int>> arr(n);
 
-    // 1. Populate the variable-sized arrays
     for (int i = 0; i < n; i++) {
         int k;
         cin >> k;
         
-        // Resize the current inner vector to hold exactly 'k' elements
         arr[i].resize(k);
         
         for (int j = 0; j < k; j++) {
@@ -27,7 +24,6 @@ int main() {
         }
     }
 
-    // 2. Process each of the 'q' queries
     for (int m = 0; m < q; m++) {
         int i, j;
         cin >> i >> j;
---
+++ problem20.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle{
+  public:
+  int width = 0, height = 0;
+
+  virtual void display(){
+    cout << width << " " << height << endl;
+  }
+};
+class RectangleArea : public Rectangle{
+public:
+   void read_input(){
+    cin >> width >> height;
+   }
+   void display(){
+    int area = 1;
+    Rectangle::display();
+    area = width * height;
+    cout << (area) ;
+   }
+};
+
+int main(){
+    RectangleArea* rec = new RectangleArea();
+    rec->read_input();
+    rec->display();
+    delete rec;
+    return 0;
+}
