5b0b575f50df445d63d637ce1ae42a8830d2bf1f
Harshaa S <harshaas21@gmail.com>
2026-08-21 10:42:50 +0530

    Problem 99

---
+++ problem99.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+   private: 
+   string name;
+   int age;
+   public:
+   Student(string name, int age){
+    this->name = name;
+    this->age = age;
+   }
+   void display(){
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+   }
+};
+
+int main(){
+    Student s("Batman", 21);
+    s.display();
+    return 0;
+}
